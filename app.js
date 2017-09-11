143938ef51a1701fab3b1190fa8d0aab2df5d222
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-11 21:07:18 +0000

    Added all callbacks to separate modules and imported and called them in app.js

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const getHTML = require('./http-functions');
+const printHTML = require('./callbacks/printHTML');
+const printUpperCase = require('./callbacks/print_uppercase');
+const printLowerCase = require('./callbacks/print_lowercase');
+const printReversed = require('./callbacks/print_reversed');
+const print1337 = require('./callbacks/print_1337');
 
 var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step5.html'
+    path: ''
 };
 
-function printHTML(data) {
-    console.log(data);
-}
+/*
+ *  Print HTML to console
+ * 
+ */
+// requestOptions.path = '/http-examples/step5.html';
+// getHTML(requestOptions, printHTML);
 
-getHTML(requestOptions, printHTML);
+
+/*
+ *  Print Upper Case
+ * 
+ */
+//  requestOptions.path = '/http-examples/step6/uppercase.html';
+//  getHTML(requestOptions, printUpperCase);
+
+
+/*
+ *  Print Lower Case
+ * 
+ */
+//  requestOptions.path = '/http-examples/step6/lowercase.html';
+//  getHTML(requestOptions, printLowerCase);
+
+/*
+ *  Print 1337 Case
+ * 
+ */
+//  requestOptions.path = '/http-examples/step6/1337.html';
+//  getHTML(requestOptions, print1337);
+
